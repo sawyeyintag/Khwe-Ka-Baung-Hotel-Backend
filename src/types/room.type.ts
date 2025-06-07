31910202e68d8c82400a56f764dc23043d053c7f
sawyeyintag <5352sawyeyintaung@gmail.com>
2025-06-08 02:05:49 +0700

    fix: Import RoomEditSchema and update its type definition

--- src/types/room.type.ts
+++ src/types/room.type.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 import { Request } from "express";
-import { RoomCreateSchema } from "../schema/room.zod";
+import { RoomCreateSchema, RoomEditSchema } from "../schema/room.zod";
 
 type RoomCreateSchema = z.infer<typeof RoomCreateSchema>;
 
-type RoomEditSchema = Omit<RoomCreateSchema, "roomNumber">;
+type RoomEditSchema = z.infer<typeof RoomEditSchema>;
 
 export interface RoomCreateSchemaRequest extends Request {
   body: RoomCreateSchema;
